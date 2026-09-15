9b9da5be313a95f3f41a18e109eda70e32e6992f
EDUARDO SIMAS ALEXANDRE <eduardo.salexandre@senacsp.edu.br>
2026-09-15 11:21:36 -0300

    Implementado CRUD de Avaliacao e Validações

---
+++ src/main/java/br/senac/tads/dsw/exemplo/model/Avaliacao.java
@@ -0,0 +1,75 @@
+package br.senac.tads.dsw.exemplo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne; // Correção: Uma avaliação tem um produto
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity // 1. Obrigatório para o JPA criar a tabela no H2
+public class Avaliacao {
+
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank 
+    private String autor;
+
+    @NotNull 
+    private String comentario;
+
+    @Min(1)
+    @Max(5)
+    private Integer nota;
+
+    @ManyToOne 
+    @JoinColumn(name = "produto_id") 
+    private Produto produto;
+    
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public Integer getNota() {
+        return nota;
+    }
+
+    public void setNota(Integer nota) {
+        this.nota = nota;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+}
